2839197be6a4b8ae0348522483977f1fe1ae5976
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-25 14:36:50 +0530

    Add files via upload

---
+++ ShiftOperators.java
@@ -0,0 +1,10 @@
+public class ShiftOperators {  
+    public static void main( String[ ] args ) {  
+        int x = 5 ;  
+        int y = x << 4 ;  
+        int v = 60; 
+        int z = v >> 4;
+        System.out.println( " The value is: " +y) ;  
+        System.out.println( " The value is: " +z) ;  
+    }  
+}  
